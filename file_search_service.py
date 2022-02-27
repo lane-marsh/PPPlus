6410082f98545e37071affeceeea8141c8ac4c40
laneMarsh <marshl@oregonstate.edu>
2022-02-27 15:26:57 -0800

    first commit with data search microservice.
    updated gui.py file to interact with data search microservicee

---
+++ file_search_service.py
@@ -0,0 +1,70 @@
+import csv
+import pickle
+import time
+from os.path import exists
+
+
+def load_instr_pickle(filename):
+    instr_fd = open(filename, 'rb+')
+    instr_dict = pickle.load(instr_fd)
+    instr_fd.close()
+    return instr_dict
+
+
+def create_new_instr_pickle(filename):
+    instr_fd = open(filename, 'wb')
+    instr_dict = {'get_data': True, 'city_name': 'Seattle'}
+    pickle.dump(instr_dict, instr_fd)
+    instr_fd.close()
+    return instr_dict
+
+
+def create_new_output_pickle(filename):
+    out_fd = open(filename, 'wb')
+    out_dict = {'city_name': instr_share['city_name'], 'done': False, 'date': [], 'price': []}
+    pickle.dump(out_dict, out_fd)
+    out_fd.close()
+
+
+def clean_output(filename):
+    out_fd = open(filename, 'rb+')
+    out_share = {'city_name': instr_share['city_name'], 'done': False, "date": [1], "price": [2]}
+    pickle.dump(out_share, out_fd)
+    out_fd.close()
+
+
+instruction_filename = 'instruction_share_microservice'
+output_filename = 'output_share_microservice'
+
+
+while True:
+    # Check if instructions file exists
+    # if exists ? load file : create file
+    if exists(instruction_filename):
+        instr_share = load_instr_pickle(instruction_filename)
+    else:
+        instr_share = create_new_instr_pickle(instruction_filename)
+    # App will request new search with dict 'get_data' boolean
+    # dict['get_data'] ?  loop through csv file : sleep()
+    if instr_share['get_data']:
+        # check if file exists, similar to instruction file
+        if exists(output_filename):
+            clean_output(output_filename)
+            out_fd = open(output_filename, 'wb')
+            with open('data/history.csv') as csvfile:
+                my_file = csv.reader(csvfile, delimiter=',')
+                header = next(my_file)
+                dates = header[5:]
+                prices = []
+                for row in my_file:
+                    if row[2] == instr_share['city_name']:
+                        prices = row[5:]
+                        out_share = {'city_name': instr_share['city_name'], 'done': True, "date": dates, "price": prices}
+                        pickle.dump(out_share, out_fd)
+                        print(out_share)
+            out_fd.close()
+
+        else:
+            create_new_output_pickle(output_filename)
+
+        time.sleep(.5)
--- gui.py
+++ gui.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+import pickle
+import time
+import os
+from PIL import ImageTk, Image
+from datetime import datetime
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from dataCrunch import ObjectifyCSV
@@ -16,14 +21,22 @@ class PPPApp(object):
         self.window.geometry("1000x700+50+50")
         self.window.title("Main Menu")
 
+        # create object for directory of images
+        self.img_dir = os.path.dirname(os.path.realpath(__file__)) + "\Images"
+        self.ppp_logo = ImageTk.PhotoImage(Image.open(self.img_dir + "\PPP_Logo.jpg"))
+
         # generate features to add to main menu
         self.make_home()
 
-        # axis values for plot of house prices over time
-        self.x_axis = []
-        self.x_ticks = []
-        self.y_axis = [i ** 2 for i in range(101)]
+        # holding place for plot objects on home search page
+        self.listing_table = None
+        self.price_plot = None
 
+        # axis values for plot of house prices over time
+        self.y_axis = []
+        self.x_axis = []
+
+        # extract and load data tables
         self.listings = ObjectifyCSV('data/listings.csv')
 
         # start the application
@@ -33,6 +46,10 @@ class PPPApp(object):
         """
         makes elements on the home screen
         """
+
+        # generate logo
+        logo_panel = tk.Label(self.window, image=self.ppp_logo)
+        logo_panel.place(x=0, y=0)
 
         # generate main label for home screen
         main_title = tk.Label(self.window,
@@ -66,13 +83,14 @@ class PPPApp(object):
         find_home_butt.place(x=50, y=150)
 
         # generate home search button
-        find_agent_butt = tk.Button(self.window,
+        interest_butt = tk.Button(self.window,
                                    width=50,
                                    height=2,
-                                   text=' Find Agent ',
+                                   text=' Interest Rate Analysis ',
                                    command=lambda: [self.window.withdraw(),
-                                                    self.open_agent_search()])
-        find_agent_butt.place(x=450, y=150)
+                                                    self.open_interest_rates()])
+        interest_butt.place(x=450, y=150)
+
 
     def open_home_search(self):
         """
@@ -84,13 +102,13 @@ class PPPApp(object):
 
         self.make_home_search(home_search)
 
-    def open_agent_search(self):
+    def open_interest_rates(self):
         """
         close current screen and open the home search screen
         """
         agent_search = tk.Toplevel(self.window)
         agent_search.geometry("1000x700+50+50")
-        agent_search.title("Agent Search")
+        agent_search.title("Interest Rate History")
 
         self.make_agent_search(agent_search)
 
@@ -106,17 +124,6 @@ class PPPApp(object):
                               wraplength=800)
         main_title.place(x=50, y=50)
 
-        # generate plot
-        price_plot = self.plot(window)
-        price_plot.place(x=400, y=150)
-
-        # plot title
-        plot_title = tk.Label(window,
-                              text='House Prices Over Time',
-                              font=('Arial', 16),
-                              bg='white')
-        plot_title.place(x=555, y=150)
-
         # generate return home button
         return_home_butt = tk.Button(window,
                                      text=' Return Home ',
@@ -124,14 +131,36 @@ class PPPApp(object):
                                                       window.destroy()])
         return_home_butt.place(x=870, y=620)
 
-        table_frame = ScrollyTable(window,
-                                   width=350,
-                                   height=500,
-                                   loc_x=30,
-                                   loc_y=150)
+        # city Search Label
+        plot_title = tk.Label(window,
+                              text='City:',
+                              font=('Arial', 16))
+        plot_title.place(x=30, y=120)
 
-        for x in range(30):
-            table_frame.add_row([x, "x", "y"])
+        # city Filter Entry
+        city_filter = tk.Entry(window)
+        city_filter.place(x=90, y=125)
+
+        # generate use filter button
+        filter_butt = tk.Button(window,
+                                     text=' Search ',
+                                     command=lambda: [self.update_listings(str(city_filter.get()), window)])
+        filter_butt.place(x=180, y=120)
+
+        self.listing_table = ScrollyTable(window,
+                                          width=350,
+                                          height=10000,
+                                          loc_x=30,
+                                          loc_y=175)
+
+        self.update_listings('Seattle', window)
+
+        heading = ScrollyTable(window,
+                               width=350,
+                               height=25,
+                               loc_x=30,
+                               loc_y=150)
+        heading.add_row(['Address', 'Bedrooms', 'Price ($)'])
 
     def make_agent_search(self, window):
         """
@@ -139,7 +168,7 @@ class PPPApp(object):
         """
 
         main_title = tk.Label(window,
-                              text='Search Agents',
+                              text='Mortgage Interest Rates',
                               font=('Arial', 36),
                               wraplength=800)
         main_title.place(x=50, y=50)
@@ -160,7 +189,7 @@ class PPPApp(object):
         plot1 = fig.add_subplot(111)
 
         # plotting the graph
-        plot1.plot(self.y_axis)
+        plot1.plot(self.x_axis, self.y_axis)
 
         # creating the Tkinter canvas
         # containing the Matplotlib figure
@@ -169,13 +198,57 @@ class PPPApp(object):
 
         return canvas.get_tk_widget()
 
-        # placing the canvas on the Tkinter window
-        # canvas.get_tk_widget().place(x=400, y=125)
-
-    def change_plot(self, city):
+    def update_listings(self, city, window):
         """
         search data and alter text criteria
         """
+
+        def update_instruction(filename, status):
+            """
+            update the instruction file for the microservice
+            inputs:
+                filename:   name of file that microservice reads from
+                status:     boolean argument. True if making request, False if confirming output file has been read
+            """
+            instr_fd = open(filename, 'wb')
+            instr_dict = {'get_data': status, 'city_name': city}
+            pickle.dump(instr_dict, instr_fd)
+            instr_fd.close()
+            return instr_dict
+
+        def read_output(filename):
+            """
+            read the output from the microservice
+            """
+            time.sleep(1)
+            instr_fd = open(filename, 'rb+')
+            instr_dict = pickle.load(instr_fd)
+            instr_fd.close()
+            return instr_dict
+
+        self.listing_table.clear_table()
+        for listing in self.listings.get_by_field({'CITY': city},
+                                                  ['ADDRESS', 'BEDS', 'PRICE']):
+            self.listing_table.add_row(listing)
+
+        update_instruction('instruction_share_microservice', True)
+        plot_data = read_output('output_share_microservice')
+
+        self.y_axis = list(map(int, plot_data['price']))
+        self.x_axis = []
+        for date in plot_data['date']:
+            self.x_axis.append(datetime.strptime(date, '%m/%d/%Y'))
+
+        # generate plot
+        self.price_plot = self.plot(window)
+        self.price_plot.place(x=400, y=150)
+
+        # plot title
+        plot_title = tk.Label(window,
+                              text='House Prices Over Time',
+                              font=('Arial', 16),
+                              bg='white')
+        plot_title.place(x=555, y=150)
 
 
 class ScrollyTable(object):
@@ -191,22 +264,38 @@ class ScrollyTable(object):
 
         self.col_1 = tk.Frame(self.main_frame, width=width/3, height=height, bg='white', padx=1, pady=1)
         self.col_1.pack(side=tk.LEFT)
+        self.col_1.pack_propagate(False)
         self.col_2 = tk.Frame(self.main_frame, width=width/3, height=height, bg='white', padx=1, pady=1)
         self.col_2.pack(side=tk.LEFT)
+        self.col_2.pack_propagate(False)
         self.col_3 = tk.Frame(self.main_frame, width=width/3, height=height, bg='white', padx=1, pady=1)
         self.col_3.pack(side=tk.LEFT)
+        self.col_3.pack_propagate(False)
+
+    def clear_table(self):
+        """
+        clear contents of the table
+        """
+
+        for widget in self.col_1.winfo_children():
+            widget.destroy()
+        for widget in self.col_2.winfo_children():
+            widget.destroy()
+        for widget in self.col_3.winfo_children():
+            widget.destroy()
 
     def add_row(self, row):
         """
         add item to the table
         """
-        obj1 = tk.Label(self.col_1, text=row[0])
+
+        obj1 = tk.Label(self.col_1, text=row[0], anchor='w', bg='white')
         obj1.pack()
 
-        obj2 = tk.Label(self.col_2, text=row[1])
+        obj2 = tk.Label(self.col_2, text=row[1], anchor='w', bg='white')
         obj2.pack()
 
-        obj3 = tk.Label(self.col_3, text=row[2])
+        obj3 = tk.Label(self.col_3, text=row[2], anchor='w', bg='white')
         obj3.pack()
 
 
